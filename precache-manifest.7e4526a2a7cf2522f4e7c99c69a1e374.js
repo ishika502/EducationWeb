d9089170e653c515f3afacd2080a950612ba7baa
ishika502 <ishikachaudhary.co@gmail.com>
2021-10-16 04:39:14 -0700

    Updates

---
+++ precache-manifest.7e4526a2a7cf2522f4e7c99c69a1e374.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc39f46b5a3d9ea30e937506d1e0d1d",
+    "url": "/EducationWeb/index.html"
+  },
+  {
+    "revision": "bff6be709e8d88fdb942",
+    "url": "/EducationWeb/static/css/2.48c23c47.chunk.css"
+  },
+  {
+    "revision": "d799fb31901280777847",
+    "url": "/EducationWeb/static/css/main.ef599880.chunk.css"
+  },
+  {
+    "revision": "bff6be709e8d88fdb942",
+    "url": "/EducationWeb/static/js/2.7a0bdc83.chunk.js"
+  },
+  {
+    "revision": "dc3d279e19407cac49931bf364bd9eda",
+    "url": "/EducationWeb/static/js/2.7a0bdc83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d799fb31901280777847",
+    "url": "/EducationWeb/static/js/main.35ada5cd.chunk.js"
+  },
+  {
+    "revision": "f574ba3635209773c8f8",
+    "url": "/EducationWeb/static/js/runtime-main.9695f54c.js"
+  },
+  {
+    "revision": "f1116f6ad9af66fd5877b131e2a65f6b",
+    "url": "/EducationWeb/static/media/app.f1116f6a.jpg"
+  },
+  {
+    "revision": "0b43915d82c2ab7783ed2a4ab8ed079b",
+    "url": "/EducationWeb/static/media/digital.0b43915d.jpg"
+  },
+  {
+    "revision": "38ecd93fce97ccc83be7170eec3695ba",
+    "url": "/EducationWeb/static/media/download.38ecd93f.jpg"
+  },
+  {
+    "revision": "d0b6892a753d30d0a2d387d67c869850",
+    "url": "/EducationWeb/static/media/market.d0b6892a.jpg"
+  },
+  {
+    "revision": "76d352ebe448d3847f4e1e286d601093",
+    "url": "/EducationWeb/static/media/software.76d352eb.jpg"
+  }
+]);
